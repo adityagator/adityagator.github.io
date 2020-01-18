c805a70100ae080844a1181998fe89de3d32bd3e
Aditya Bharadwaj <adityabharadwaj@Adityas-MacBook-Pro.local>
2020-01-17 21:50:30 -0500

    Home screen down button

--- main.js
+++ main.js
@@ -20,6 +20,12 @@ $(document).ready(function () {
         }, 2000);
     });
 
+    $('a[href*="#"]').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500);
+    });
+
     $('#up').on('click', function(){
         $('html, body').animate({
             scrollTop: 0
